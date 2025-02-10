016861f8bb11cb67c1b8f356f460d63383b7e5be
Kamila Matla-Tomczyk <ka1130@o2.pl>
2025-02-10 12:14:48 +0100

    Add linters

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-prettier/recommended",
+    "stylelint-config-astro",
+  ],
+  // plugins: [],
+  rules: {},
+};
